0690f69be9fbce89d68c87e577e782009dec7490
manighahrmani <mashapicasso@gmail.com>
2023-12-19 02:56:58 +0000

    Add greet function and convert temperature functions

--- lect15.dart
+++ lect15.dart
@@ -1,8 +1,20 @@
+void main() {
+  String myName = 'Alice';
+  greet(myName);
+}
+
 void greet(String name) {
   print('Hello, $name!');
 }
 
-void main() {
-  String myName = 'Alice';
-  greet(myName);
+double celsiusToFahrenheit(double celsius) {
+  double fahrenheit = celsius * 9 / 5 + 32;
+  return fahrenheit;
+}
+
+int mystery(int num) {
+  num++;
+  num--;
+  num ~/= 2;
+  return num;
 }
