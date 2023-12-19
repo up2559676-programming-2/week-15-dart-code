2600a68e712300ee207318e9b543c853779eb205
manighahrmani <mashapicasso@gmail.com>
2023-12-19 03:19:42 +0000

    Add secret function to perform calculations

--- lect15.dart
+++ lect15.dart
@@ -1,6 +1,11 @@
 void main() {
-  String myName = 'Alice';
-  greet(myName);
+  // String myName = 'Alice';
+  // greet(myName);
+
+  int a = 5;
+  double b = 10.0;
+  double result = secret(a, b);
+  print(result);
 }
 
 void greet(String name) {
@@ -17,4 +22,14 @@ int mystery(int num) {
   num--;
   num ~/= 2;
   return num;
+}
+
+double secret(int x, double y) {
+  x += 5;
+  y /= 5;
+  x ~/= 3;
+  y *= 2;
+  x++;
+  double result = x * y;
+  return result;
 }
