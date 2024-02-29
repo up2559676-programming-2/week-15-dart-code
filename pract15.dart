c41e460c1ac48c253f619182da06106475634580
manighahrmani <mashapicasso@gmail.com>
2024-02-29 10:43:32 +0000

    Add speedCalculator function

--- pract15.dart
+++ pract15.dart
@@ -4,23 +4,15 @@ import 'dart:math';
 void main() {
   int result = multiplyBy2(5);
   print('Result: $result');
-
-  // int distance = 100;
-  // double time = 9.58;
-  // speedCalculator(distance, time);
-
-  // typeConversion();
-
-  // String greetings = getGreetings();
-  // print(greetings);
-
-  // askForStudentNumber();
-
-  // circumferenceOfCircle();
 }
 
 int multiplyBy2(int number) {
   return number * 2;
+}
+
+void speedCalculator(double distance, double time) {
+  double speed = distance / time;
+  print('Speed: $speed');
 }
 
 void typeConversion() {
